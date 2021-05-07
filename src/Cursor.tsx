e5b072ad78d0ceca9ff937840ce1c60c6280a7cd
camilossantos2809 <camilossantos.2809@gmail.com>
2021-05-06 21:36:26 -0300

    criação dos cursores (círculos nas extremidades do arco)

--- src/Cursor.tsx
+++ src/Cursor.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import Animated, { useAnimatedProps } from "react-native-reanimated";
-import { polar2Canvas } from "react-native-redash";
+import { polar2Canvas, Vector } from "react-native-redash";
 import { Circle } from "react-native-svg";
 
 import { STROKE, R, CENTER } from "./Constants";
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
-interface CursorProps {}
+interface CursorProps {
+  pos: Animated.SharedValue<Vector>;
+}
 
-const Cursor = ({ theta }: CursorProps) => {
-  return null;
+const Cursor = ({ pos }: CursorProps) => {
+  const animatedProps = useAnimatedProps(() => ({
+    cx: pos.value.x,
+    cy: pos.value.y,
+  }));
+  return (
+    <AnimatedCircle r={STROKE / 2} animatedProps={animatedProps} fill="blue" />
+  );
 };
 
 export default Cursor;
